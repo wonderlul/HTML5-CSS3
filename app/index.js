f627c1fd618e4e1e3ed4278e3718cedd5127ea87
lulek92 <lulmichal@gmail.com>
2020-09-03 14:51:06 +0200

    ADDED JS

--- app/index.js
+++ app/index.js
@@ -7,3 +7,137 @@ import "../assets/styles/index.scss";
 // ================================
 // START YOUR APP HERE
 // ================================
+
+//Swipe
+
+const ACTION_REMOVE_FOCUS_FROM_FEATURES_LIST = (listItems) => {
+    const listItemWithClassToRemove = listItems.find((listItem) => listItem.querySelector(".features-list__active"));
+    if (listItemWithClassToRemove) {
+        listItemWithClassToRemove.querySelector(".features-list__active").classList.remove("features-list__active");
+    }
+};
+
+const featuresListItems = [...document.querySelectorAll(".features-list__list-item")];
+
+const leftArrow = document.querySelector(".left-arrow");
+const rightArrow = document.querySelector(".right-arrow");
+
+const featuresHeaders = [...document.querySelectorAll(".features-list__header")];
+const featuresParagraphs = [...document.querySelectorAll(".features-list__paragraph")];
+const featureContent = featuresHeaders.map((header, index) => {
+    return {
+        header: header,
+        paragraph: featuresParagraphs[index],
+    };
+});
+
+const featuresHeaderTemplate = document.querySelector(".features__header");
+const featuresParagraphTemplate = document.querySelector(".features__paragraph");
+const featuresTemplates = [featuresHeaderTemplate, featuresParagraphTemplate];
+
+let ordinalNumber = 0;
+
+const fadeOut = (templates) => {
+    templates.forEach((template) => {
+        template.style.opacity = 0;
+        template.style.transition = "0.5s";
+    });
+};
+
+const fadeIn = (templates) => {
+    templates.forEach((template) => {
+        template.style.opacity = 1;
+    });
+};
+
+const ACTION_SWITCH_CONTENT = (content) => {
+    const header = content[ordinalNumber].header["textContent"];
+    const paragraph = content[ordinalNumber].paragraph["textContent"];
+
+    featuresHeaderTemplate.textContent = header;
+    featuresParagraphTemplate.textContent = paragraph;
+};
+
+const ACTION_SWITCH_CONTENT_WITH_ANIMATION = (ACTION_SWITCH_CONTENT) => {
+    fadeOut(featuresTemplates);
+
+    setTimeout(() => ACTION_SWITCH_CONTENT(featureContent), 500);
+
+    setTimeout(() => fadeIn(featuresTemplates), 500);
+};
+
+const ACTION_ORDINAL_NUMBER_UPDATE = (direction, content) => {
+    switch (direction) {
+        case "left":
+            if (ordinalNumber === 0) {
+                ordinalNumber = content.length - 1;
+            } else {
+                ordinalNumber -= 1;
+            }
+            break;
+
+        case "right":
+            if (ordinalNumber === content.length - 1) {
+                ordinalNumber = 0;
+            } else {
+                ordinalNumber += 1;
+            }
+            break;
+
+        default:
+            ordinalNumber += 0;
+            break;
+    }
+};
+
+const ACTION_SWIPE = (direction) => {
+    ACTION_ORDINAL_NUMBER_UPDATE(direction, featureContent);
+
+    ACTION_SWITCH_CONTENT_WITH_ANIMATION(ACTION_SWITCH_CONTENT);
+
+    ACTION_REMOVE_FOCUS_FROM_FEATURES_LIST(featuresListItems);
+};
+
+leftArrow.addEventListener("click", () => ACTION_SWIPE("left"));
+rightArrow.addEventListener("click", () => ACTION_SWIPE("right"));
+
+//Navbar tabs
+
+const navbarTabs = [...document.querySelectorAll(".navbar-desktop__item")];
+
+const ACTION_NAVBAR_FOCUS = (tab, navbarTabs) => {
+    const tabToRemoveClass = navbarTabs.find((tab) => tab.classList.contains("nav-active-tab"));
+
+    tabToRemoveClass.classList.remove("nav-active-tab");
+    tab.classList.add("nav-active-tab");
+};
+
+navbarTabs.forEach((tab) => tab.addEventListener("click", () => ACTION_NAVBAR_FOCUS(tab, navbarTabs)));
+
+//Features list
+
+const ACTION_FEATURES_LIST_FOCUS = (listItem, listItems) => {
+    const header = listItem.querySelector(".features-list__header");
+
+    header.classList.add("features-list__active");
+
+    ordinalNumber = listItems.indexOf(listItem);
+
+    ACTION_SWITCH_CONTENT_WITH_ANIMATION(ACTION_SWITCH_CONTENT);
+};
+
+featuresListItems.forEach((listItem) => {
+    listItem.addEventListener("click", () => ACTION_FEATURES_LIST_FOCUS(listItem, featuresListItems));
+});
+
+(function () {
+    const tabs = [...document.querySelectorAll(".contact__navbar-item")];
+
+    tabs.forEach((tab) =>
+        tab.addEventListener("click", () => {
+            const tabToRemoveClass = tabs.find((tab) => tab.classList.contains("contact-active-tab"));
+            tabToRemoveClass.classList.remove("contact-active-tab");
+            tab.classList.add("contact-active-tab");
+        })
+    );
+})();
